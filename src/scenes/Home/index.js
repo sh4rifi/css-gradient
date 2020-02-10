9337e91d2db5d336e404b61205441cf08424154e
Kourosh Neyestani <kourosh.neyestanigmail.com>
2020-02-10 17:00:26 +0330

    Added Rangle Slider for angle

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -4,31 +4,49 @@ import { ChromePicker } from 'react-color';
 class Home extends React.Component {
 
     state = {
+        type: 'linear-gradient',
+        angle: 0,
         color1: 'red',
         color2: 'blue',
     };
 
-    handleClickSelectColor = (type, value) => {
+    handleClick = (type, value) => {
         this.setState({
             [type]: value.hex
         })
     }
 
+    handleChange = (type, event) => {
+        this.setState({
+            [type]: event.target.value
+        })
+    }
+
     render() {
-        const { color1, color2 } = this.state;
-        const gradient = `linear-gradient(60deg, ${color1}, ${color2})`;
+        const { type, angle, color1, color2 } = this.state;
+        const gradient = `${type}(${angle}deg, ${color1}, ${color2})`;
         return (
             <div style={{ backgroundImage: gradient, height: '900px' }}>
-                <div class='color-picker color-picker-1'>
+                <div className='option color-picker color-picker-1'>
                     <ChromePicker
                         color={color1}
-                        onChange={(color) => this.handleClickSelectColor('color1', color)}
+                        onChange={(color) => this.handleClick('color1', color)}
                     />
                 </div>
-                <div class='color-picker color-picker-2'>
+                <div className='option color-picker color-picker-2'>
                     <ChromePicker
                         color={color2}
-                        onChange={(color) => this.handleClickSelectColor('color2', color)}
+                        onChange={(color) => this.handleClick('color2', color)}
+                    />
+                </div>
+                <div className='option range-slider'>
+                    <input
+                        min="-180"
+                        max="180"
+                        step="1"
+                        type="range"
+                        value={angle}
+                        onChange={(event) => this.handleChange('angle', event)}
                     />
                 </div>
                 <div className='output'>
