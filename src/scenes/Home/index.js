a0a26224fb4b65178503c32e3dd24c66ac871614
Kourosh Neyestani <kourosh.neyestanigmail.com>
2020-02-10 16:20:45 +0330

    Added Color Pickers

--- src/scenes/Home/index.js
+++ src/scenes/Home/index.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import { ChromePicker } from 'react-color';
 
 class Home extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            color1: 'white',
-            color2: 'black'
-        }
-    }
+    state = {
+        color1: 'red',
+        color2: 'blue',
+    };
 
     handleClickSelectColor = (type, value) => {
         this.setState({
-            [type]: value
+            [type]: value.hex
         })
     }
 
     render() {
         const { color1, color2 } = this.state;
-        const gradient = `linear-gradient(${color1}, ${color2})`;
+        const gradient = `linear-gradient(60deg, ${color1}, ${color2})`;
         return (
-            <div style={{backgroundImage: gradient, height: '900px'}}>
-                <button onClick={() => this.handleClickSelectColor('color1', 'blue')}>Button 1</button>
-                <button onClick={() => this.handleClickSelectColor('color2', 'red')}>Button 2</button>
+            <div style={{ backgroundImage: gradient, height: '900px' }}>
+                <div class='color-picker color-picker-1'>
+                    <ChromePicker
+                        color={color1}
+                        onChange={(color) => this.handleClickSelectColor('color1', color)}
+                    />
+                </div>
+                <div class='color-picker color-picker-2'>
+                    <ChromePicker
+                        color={color2}
+                        onChange={(color) => this.handleClickSelectColor('color2', color)}
+                    />
+                </div>
                 <div className='output'>
                     background-image: {gradient}
                 </div>
